2ba589eecce7fa021a514a4b8c0c5a389f64bbfa
itwaysindia <143081902+itwaysindia@users.noreply.github.com>
2024-07-29 09:49:10 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,50 @@
+/*
+Type of Functions in JS
+
+1. Functional Declaration
+2. Function Expression
+3. Arrow Function
+4. Anonymous Functions
+5. IIFE - Immediately Invoked Function Expressions
+6. High-Order Functions
+7. Callback Functions
+
+*/
+
+//Function Declaration
+/* 
+A function declaration defines a function with a specified name. this type of function can be called before its declaration due to hoisting.
+*/
+
+
+function functionName(param, param2){
+console.log("Hello This is", param);
+
+console.log("From", param2);
+}
+
+let arg1 = "ÄBC";
+let arg2 = "MP";
+functionName(arg1, arg2);
+
+
+
+// Function Expression
+
+const functionExp = function(){
+
+}
+functionExp();
+
+
+// km = 
+// avg =
+// fuel_cost =
+// toll = 
+
+
+// fuel consume = ?
+// fare with toll=?
+// fare without toll=?
+
+
