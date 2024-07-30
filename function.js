d14af81b138b01c1a5b8bd34a5c8c0e966a12d7a
itwaysindia <143081902+itwaysindia@users.noreply.github.com>
2024-07-30 09:46:16 +0530

    Add files via upload

--- function.js
+++ function.js
@@ -25,7 +25,7 @@ console.log("From", param2);
 
 let arg1 = "ÄBC";
 let arg2 = "MP";
-functionName(arg1, arg2);
+// functionName(arg1, arg2);
 
 
 
@@ -34,7 +34,7 @@ functionName(arg1, arg2);
 const functionExp = function(){
 
 }
-functionExp();
+// functionExp();
 
 
 // km = 
@@ -48,3 +48,38 @@ functionExp();
 // fare without toll=?
 
 
+
+function add(val1, val2){
+sum = val1+val2;
+return sum;
+// console.log("Hello");
+
+}
+
+// const add = function (val1, val2){
+// // let sum = val1+val2;
+// console.log(val1+val2);
+// }
+
+let a = add(20,10);
+// consolse.log(a);
+
+
+// function to find out tax of 18 %
+// function to provide discount after tax in %.
+
+function tax(value,tax){
+let taxValue = (value*tax)/100;
+return value+taxValue;
+}
+
+function discount(discountValue, per){
+let disValue = (discountValue*per)/100;
+return discountValue-disValue;
+}
+
+
+let taxVal = tax(100,18);
+console.log(taxVal);
+let netVal = discount(taxVal, 10);
+console.log(netVal);
